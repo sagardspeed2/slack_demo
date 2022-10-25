359d693ea65a6f440ff040c7c964beb8867235cc
Sagar Davara <sagardspeed2@gmail.com>
2022-10-25 23:01:07 +0530

    fix: added response in hook

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  res.status(200).send("Hello");
+  res.status(200).send(`Hello ${req.body.text}`);
 });
 
 app.listen(3000, () => {
